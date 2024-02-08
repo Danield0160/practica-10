3c0f32181342e2879c2da2f387de2ddfcc9b8029
Daniel windows <daniel.rosles@gmail.com>
2024-02-08 22:35:16 +0000

    boton carousel

--- js/script.js
+++ js/script.js
@@ -59,29 +59,39 @@ function carousel() {
     carousel_div.append(image)
     imagenes_div.push(image)
 
+    var indice_global 
+    async function animacion(imagenes, indice ,unico=false) {
+        if(indice){indice_global = indice}else{indice = indice_global}
 
-    async function animacion(imagenes, indice = 1) {
-        let espera = new Promise(function (resolve) {
+        await new Promise(function (resolve) {
             for (let imagen of imagenes) {
                 imagen.animate({ left: (-45 * indice) + "vh" }, 1000, () => resolve())
             }
         })
-
-        await espera
+        if(unico){return}
         await new Promise(function (resolve) {
             setTimeout(() => resolve(), 2200)
         })
 
-        if (indice == imagenes.length - 1) {
+        if (indice_global > imagenes.length -2) {
             for (let imagen of imagenes) {
                 imagen.css("left", "0")
             }
-            animacion(imagenes)
+            animacion(imagenes,1)
         } else {
-            animacion(imagenes, indice + 1)
+            animacion(imagenes, Number(indice_global) + 1)
         }
     }
-    animacion(imagenes_div)
+    animacion(imagenes_div,1)
+
+    let container = $(document.createElement("div")).attr("id","carousel_controller")
+    for (let indice = 1; indice < imagenes_div.length; indice++) {        
+        let boton = $(document.createElement("button"))
+        boton.text(indice)
+        boton.on("click", function () { animacion(imagenes_div, indice,true) })
+        container.append(boton)
+    }
+    $("#cuerpo").append(container)
 }
 carousel()
 
@@ -90,7 +100,7 @@ var actual
 $(".multiple").hover(function (e) {
     actual = e.target
     $(actual).removeClass("oculto")
-},function (e) {
+}, function (e) {
     $(actual).addClass("oculto")
-    }
+}
 )
